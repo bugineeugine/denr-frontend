e5fa98bd51f1d142e903e959348d628dde109b86
Eugine Rosillon <bugineeugine06@gmail.com>
2026-01-04 19:49:37 +0800

    reverse

--- src/components/Timeline/History.tsx
+++ src/components/Timeline/History.tsx
@@ -74,7 +74,7 @@ const History = ({ permit }: { permit: PermitDataType }) => {
           },
         }}
       >
-        {steps.map((label, index) => {
+        {[...steps].reverse().map((label, index) => {
           const findData = data?.data[index];
           const isCompleted = index < currentStep;
           const isCurrent = index === currentStep;
